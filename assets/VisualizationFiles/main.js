7a68592c24bcafc6b47e6ef026f48174555a8f74
Rubens Jr <147972854+rufferbaraldi@users.noreply.github.com>
2025-06-01 09:39:54 +0200

    Update main.js

--- assets/VisualizationFiles/main.js
+++ assets/VisualizationFiles/main.js
@@ -1,11 +1,10 @@
 let map = L.map('map').setView([-30.0, -53.0], 6);
 
 // Base map layer
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
+L.tileLayer('https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}{r}.png?apikey={apikey}', {
+	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	apikey: '<your apikey>',
+	maxZoom: 22
 }).addTo(map);
 
 let currentLayer;
